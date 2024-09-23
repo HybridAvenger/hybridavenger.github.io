eb8d38475d91a3f577406d1038ee7ea6f155cb6d
HybridAvenger69 <hybridavenger69@yahoo.com>
2024-09-23 10:38:08 -0600

    commitment

---
+++ test/js/skyrim.js
@@ -0,0 +1,9 @@
+// Load the JSON file containing the mod list
+$.getJSON('mods.json', function(mods) {
+  // Add each mod to the mod list
+mods.forEach(function(mod) {
+    var modElement = `
+        <div class="mod">
+            <img src="${mod.image}" alt="${mod.name}" width="200">
+            <h3>${mod.name}</h3>
+            <p>${mod.description}</
